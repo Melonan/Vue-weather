60ece844c19c844afb12ae13a103d2771d905a00
Lonan <1422872473@qq.com>
2021-10-26 00:02:45 +0800

    first time commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search" placeholder="Search...">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      api_key:'0e7dd38e74e7e9b0df869da17ae29ed6'
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
